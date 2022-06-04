afd53c93240f180c26256faf8f55834f692a1964
JustSorrow <34898438+xXCaulDevsYT@users.noreply.github.com>
2022-06-04 18:06:54 -0500

    Create interactionCreate.js

---
+++ events/client/interactionCreate.js
@@ -0,0 +1,31 @@
+module.exports = {
+    name: 'interactionCreate',
+
+    /**
+     * @param {CommandInteraction} interaction 
+     * @param {Client} client 
+     */
+    async execute(interaction, client) {
+        if (!interaction.isCommand()) return;
+        
+        const command = client.slash.get(interaction.commandName);
+        if (!command) return interaction.reply({ content: 'an Error check console' });
+        
+        const args = [];
+        
+        for (let option of interaction.options.data) {
+            if (option.type === 'SUB_COMMAND') {
+                if (option.name) args.push(option.name);
+                option.options?.forEach(x => {
+                    if (x.value) args.push(x.value);
+                });
+            } else if (option.value) args.push(option.value);
+        }
+        
+        try {
+            command.run(client, interaction, args)
+        } catch (e) {
+            interaction.reply({ content: e.message });
+        }
+    }
+}
