c3f360ef13d66bfb9595c8ce9e4b47b99eb49df5
JustSorrow <34898438+xXCaulDevsYT@users.noreply.github.com>
2022-06-04 18:05:30 -0500

    Create messageCreate.js

---
+++ events/client/messageCreate.js
@@ -0,0 +1,17 @@
+module.exports = {
+    name: 'messageCreate',
+
+    /**
+     * @param {Message} message 
+     * @param {Client} client 
+     */
+    async execute(message, client) {
+        if (message.author.bot || !message.guild || !message.content.toLowerCase().startsWith(client.config.botPrefix)) return;
+        const [cmd, ...args] = message.content.slice(client.config.botPrefix.length).trim().split(" ");
+        const command = client.commands.get(cmd.toLowerCase()) || client.commands.find(c => c.aliases?.includes(cmd.toLowerCase()));
+        
+        if (!command) return;
+        
+        await command.run(client, message, args);
+    }
+}
