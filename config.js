d48fec1b64277bf0e0dcde52003669db1df2b0f2
JustSorrow <34898438+xXCaulDevsYT@users.noreply.github.com>
2022-06-04 18:12:29 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    botClientID: "YOUR_DISCORD_BOT_ID",
+    botPrefix: "!",
+    ownerID: "YOUR_USER_ID",
+    embedColor: "#FFFFFF",
+    embedfooterText: "Discord.js v13 bot example by Breakcord Development."
+}
